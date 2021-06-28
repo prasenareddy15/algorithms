c7d79d7234f4413b825468a481abaf1ce25b057d
prasenareddy15 <66769276+prasenareddy15@users.noreply.github.com>
2021-06-28 10:45:23 +0530

    stack with lists

---
+++ progamiz/dsa_1/stack.py
@@ -0,0 +1,20 @@
+def create_stack():
+   stack=[]
+   return stack
+
+def check_empty(stack):
+   return(len(stack)==0)
+def push(stack,ele):
+   stack.append(ele)
+   print("pushed the item:",ele)
+def pop(stack):
+   if(check_empty(stack)):
+      return("There are no elements to pop...\nThe stack is empty!!")
+   return(stack.pop())
+stack = create_stack()
+push(stack, str(1))
+push(stack, str(2))
+push(stack, str(3))
+push(stack, str(4))
+print("popped item: " + pop(stack))
+print("stack after popping an element: " + str(stack)) 
